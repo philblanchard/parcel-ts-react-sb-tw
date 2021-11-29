ed916ac47960d3592cc1a893ff19ce7382c78baa
Phil Blanchard <phil@philblanchard.com>
2021-11-29 12:07:11 -0500

    template structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# parcel-ts-react-sb-tailwind
---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import './styles.css'
+
+const Button = () => {
+
+    return (
+        <div className="rounded-full py-3 px-6 bg-pink-500 w-20">Click Me</div>
+    )
+}
+
+export default Button;
